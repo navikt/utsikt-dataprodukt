2372177b9299b905025db5752882797d4747e945
Linda J. Claesson <linda.josephine.claesson@nav.no>
2025-07-21 14:46:17 +0200

    per dag istedenfor per måned og ma_handteres_manuelt som string, for riktig type for metabase

--- dbt_utsikt/models/marts/antall_venteregister_beregninger_per_ventestatus_per_dag.sql
+++ dbt_utsikt/models/marts/antall_venteregister_beregninger_per_ventestatus_per_dag.sql
@@ -9,10 +9,8 @@ select
 count(sts.beregning_id) antall_beregninger
 ,ventestatus
 ,sts.ventestatus_id
-,ma_handteres_manuelt
-,extract(month from ber.beregnet_dato) as maned
-,extract(year from ber.beregnet_dato) as ar
+,cast(ma_handteres_manuelt as string) as ma_handteres_manuelt
 from distinkte_beregninger_per_ventestatus sts
 left join {{ ref('int_venteregister_statuskoder_manuell_handtering') }} stk on sts.ventestatus_id = stk.ventestatus_id
 left join {{ ref('stg_db2os__venteregister_beregninger') }} ber on sts.beregning_id = ber.beregning_id
-group by ventestatus, sts.ventestatus_id, ma_handteres_manuelt, maned, ar
+group by ventestatus, sts.ventestatus_id, ma_handteres_manuelt, beregnet_dato
