8bfc3c7c235a8e88be5aafa6a4f2ce29a6ca3b2a
Linda J. Claesson <linda.josephine.claesson@nav.no>
2025-07-23 11:40:02 +0200

    ved uhell fjerna beregnet_dato, så må legge dette inn igjen

--- dbt_utsikt/models/marts/antall_venteregister_beregninger_per_ventestatus_per_dag.sql
+++ dbt_utsikt/models/marts/antall_venteregister_beregninger_per_ventestatus_per_dag.sql
@@ -10,6 +10,7 @@ count(sts.beregning_id) antall_beregninger
 ,ventestatus
 ,sts.ventestatus_id
 ,cast(ma_handteres_manuelt as string) as ma_handteres_manuelt
+,beregnet_dato
 from distinkte_beregninger_per_ventestatus sts
 left join {{ ref('int_venteregister_statuskoder_manuell_handtering') }} stk on sts.ventestatus_id = stk.ventestatus_id
 left join {{ ref('stg_db2os__venteregister_beregninger') }} ber on sts.beregning_id = ber.beregning_id
