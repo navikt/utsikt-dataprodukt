ef64552bd9a65092b2172d9e48fff79898a06c9c
Linda J. Claesson <linda.josephine.claesson@nav.no>
2025-07-24 11:25:10 +0200

    la til faggruppe i tillegg til fagomrader i mart

--- dbt_utsikt/models/marts/antall_venteregister_beregninger_per_fagomrade_per_dag.sql
+++ dbt_utsikt/models/marts/antall_venteregister_beregninger_per_fagomrade_per_dag.sql
@@ -10,6 +10,7 @@ beregning_id_per_fagomrade_per_beregnet_dato as (
 select
 stpn.beregning_id
 ,fagomrade
+,faggruppe
 ,beregnet_dato
 from dist_beregning_fagomrade as stpn 
 left join {{ ref('int_fagomrader_med_tilhorende_faggrupper') }} fag on stpn.fagomrade_id = fag.fagomrade_id
@@ -19,8 +20,9 @@ left join {{ ref('stg_db2os__venteregister_beregninger') }} brg on brg.beregning
 select
 count(beregning_id) antall_beregninger
 ,fagomrade
+,faggruppe
 ,beregnet_dato
 from beregning_id_per_fagomrade_per_beregnet_dato
-group by fagomrade, beregnet_dato
+group by fagomrade, faggruppe, beregnet_dato
 
 --legg merke til at en beregning_id kan tilhøre flere fagomrader, så en beregning kan telle i flere antall_beregninger per fagområde per dag
