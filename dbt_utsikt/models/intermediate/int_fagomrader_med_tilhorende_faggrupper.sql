73b8e95be912292dfb51113c318fbe3358a6c69e
caroliwi <caroline.winther@nav.no>
2025-09-23 16:28:38 +0200

    endret litt på format på modeller

--- dbt_utsikt/models/intermediate/int_fagomrader_med_tilhorende_faggrupper.sql
+++ dbt_utsikt/models/intermediate/int_fagomrader_med_tilhorende_faggrupper.sql
@@ -1,10 +1,42 @@
 -- int_fagomrader_med_tilhorende_faggrupper
-select
-    fagomrade_kode,
-    fagomrade_navn,
-    omr.faggruppe_kode,
-    faggruppe_navn
-from {{ ref('stg_db2os__fagomrader') }} as omr
-left join
-    {{ ref('stg_db2os__faggrupper') }} as grp
-    on omr.faggruppe_kode = grp.faggruppe_kode
+with
+
+ref_stg_db2os__fagomrader as (
+    select
+        fagomrade_kode,
+        fagomrade_navn,
+        faggruppe_kode
+    from {{ ref('stg_db2os__fagomrader') }}
+),
+
+ref_stg_db2os__faggrupper as (
+    select
+        faggruppe_kode,
+        faggruppe_navn
+    from {{ ref('stg_db2os__faggrupper') }}
+),
+
+join_omrade_gruppe as (
+    select
+        ref_stg_db2os__fagomrader.fagomrade_kode,
+        ref_stg_db2os__fagomrader.fagomrade_navn,
+        ref_stg_db2os__faggrupper.faggruppe_kode,
+        ref_stg_db2os__faggrupper.faggruppe_navn
+    from ref_stg_db2os__fagomrader
+    left join
+        ref_stg_db2os__faggrupper
+        on
+            ref_stg_db2os__fagomrader.faggruppe_kode
+            = ref_stg_db2os__faggrupper.faggruppe_kode
+),
+
+final as (
+    select
+        fagomrade_kode,
+        fagomrade_navn,
+        faggruppe_kode,
+        faggruppe_navn
+    from join_omrade_gruppe
+)
+
+select * from final
--- dbt_utsikt/models/marts/antall_beregninger_per_faggruppe_per_dag.sql
+++ dbt_utsikt/models/marts/antall_beregninger_per_faggruppe_per_dag.sql
@@ -1,10 +1,46 @@
-select
-    brg.faggruppe_kode,
-    faggruppe_navn,
-    beregnet_dato,
-    count(beregning_id) as antall_beregninger
-from {{ ref('stg_db2os__beregninger') }} as brg
-left join
-    {{ ref('stg_db2os__faggrupper') }} as fag
-    on brg.faggruppe_kode = fag.faggruppe_kode
-group by brg.faggruppe_kode, faggruppe_navn, beregnet_dato
+--antall_beregninger_per_faggruppe_per_dag
+with
+ref_stg_db2os__beregninger as (
+    select
+        beregning_id,
+        faggruppe_kode,
+        beregnet_dato
+    from {{ ref('stg_db2os__beregninger') }}
+),
+
+ref_stg_db2os__faggrupper as (
+    select
+        faggruppe_kode,
+        faggruppe_navn
+    from {{ ref('stg_db2os__faggrupper') }}
+),
+
+antall_beregninger_per_faggruppe_per_dag as (
+    select
+        ref_stg_db2os__beregninger.faggruppe_kode,
+        ref_stg_db2os__faggrupper.faggruppe_navn,
+        ref_stg_db2os__beregninger.beregnet_dato,
+        count(ref_stg_db2os__beregninger.beregning_id) as antall_beregninger
+    from ref_stg_db2os__beregninger
+    left join
+        ref_stg_db2os__faggrupper
+        on
+            ref_stg_db2os__beregninger.faggruppe_kode
+            = ref_stg_db2os__faggrupper.faggruppe_kode
+    group by
+        ref_stg_db2os__beregninger.faggruppe_kode,
+        ref_stg_db2os__faggrupper.faggruppe_navn,
+        ref_stg_db2os__beregninger.beregnet_dato
+
+),
+
+final as (
+    select
+        faggruppe_kode,
+        faggruppe_navn,
+        beregnet_dato,
+        antall_beregninger
+    from antall_beregninger_per_faggruppe_per_dag
+)
+
+select * from final
