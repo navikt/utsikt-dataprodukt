3a99e5a4d8163fe3318c0080416247ee26da0036
caroliwi <caroline.winther@nav.no>
2025-09-24 14:55:33 +0200

    endret formattering og oppsett på modeller så det blir mer lesbart (for caroline i alle fall)

--- dbt_utsikt/models/marts/antall_beregninger_per_fagomrade_per_dag.sql
+++ dbt_utsikt/models/marts/antall_beregninger_per_fagomrade_per_dag.sql
@@ -1,32 +1,67 @@
-with dist_beregning_fagomrade as (
-    select distinct
+--antall_beregninger_per_fagomrade_per_dag
+
+with
+
+ref_stg_db2os__stoppnivaer as (
+    select
         beregning_id,
         fagomrade_kode
     from {{ ref('stg_db2os__stoppnivaer') }}
-    group by beregning_id, fagomrade_kode
+),
+
+ref_int_fagomrader_med_tilhorende_faggrupper as (
+    select
+        fagomrade_kode,
+        fagomrade_navn,
+        faggruppe_navn
+    from {{ ref('int_fagomrader_med_tilhorende_faggrupper') }}
+),
+
+ref_stg_db2os__beregninger as (
+    select
+        beregning_id,
+        beregnet_dato
+    from {{ ref('stg_db2os__beregninger') }}
+),
+
+dist_beregning_fagomrade as (
+    select distinct
+        beregning_id,
+        fagomrade_kode
+    from ref_stg_db2os__stoppnivaer
 ),
 
 beregning_id_per_fagomrade_per_beregnet_dato as (
     select
-        stpn.beregning_id,
+        dist_beregning_fagomrade.beregning_id,
+        ref_int_fagomrader_med_tilhorende_faggrupper.fagomrade_navn,
+        ref_int_fagomrader_med_tilhorende_faggrupper.faggruppe_navn,
+        ref_stg_db2os__beregninger.beregnet_dato
+    from dist_beregning_fagomrade
+    left join
+        ref_int_fagomrader_med_tilhorende_faggrupper
+        on
+            dist_beregning_fagomrade.fagomrade_kode
+            = ref_int_fagomrader_med_tilhorende_faggrupper.fagomrade_kode
+    left join
+        ref_stg_db2os__beregninger
+        on
+            dist_beregning_fagomrade.beregning_id
+            = ref_stg_db2os__beregninger.beregning_id
+),
+
+final as (
+    select
         fagomrade_navn,
         faggruppe_navn,
-        beregnet_dato
-    from dist_beregning_fagomrade as stpn
-    left join
-        {{ ref('int_fagomrader_med_tilhorende_faggrupper') }} as fag
-        on stpn.fagomrade_kode = fag.fagomrade_kode
-    left join
-        {{ ref('stg_db2os__beregninger') }} as brg
-        on stpn.beregning_id = brg.beregning_id
+        beregnet_dato,
+        count(beregning_id) as antall_beregninger
+    from beregning_id_per_fagomrade_per_beregnet_dato
+    group by fagomrade_navn, faggruppe_navn, beregnet_dato
 )
 
-select
-    fagomrade_navn,
-    faggruppe_navn,
-    beregnet_dato,
-    count(beregning_id) as antall_beregninger
-from beregning_id_per_fagomrade_per_beregnet_dato
-group by fagomrade_navn, faggruppe_navn, beregnet_dato
+select * from final
 
---legg merke til at en beregning_id kan tilhøre flere fagomrader, så en beregning kan telle i flere antall_beregninger per fagområde per dag
+
+--legg merke til at en beregning_id kan tilhøre flere fagomrader, 
+--så en beregning kan telle i flere antall_beregninger per fagområde per dag
--- dbt_utsikt/models/marts/antall_beregninger_per_ventestatus_per_dag.sql
+++ dbt_utsikt/models/marts/antall_beregninger_per_ventestatus_per_dag.sql
@@ -1,28 +1,71 @@
-with distinkte_beregninger_per_ventestatus as (
+with
+ref_stg_db2os__stoppstatuser as (
+    select
+        beregning_id,
+        ventestatus_kode
+    from {{ ref('stg_db2os__stoppstatuser') }}
+),
+
+ref_int_statuskoder_manuell_handtering as (
+    select
+        ventestatus_kode,
+        ventestatus_beskrivelse,
+        handteres_manuelt
+    from {{ ref('int_statuskoder_manuell_handtering') }}
+),
+
+ref_stg_db2os__beregninger as (
+    select
+        beregning_id,
+        beregnet_dato
+    from {{ ref('stg_db2os__beregninger') }}
+),
+
+distinkte_beregninger_per_ventestatus as (
     select distinct
         ventestatus_kode,
-        (beregning_id)
-    from {{ ref('stg_db2os__stoppstatuser') }}
+        beregning_id
+    from ref_stg_db2os__stoppstatuser
+),
+
+antall_beregninger_per_ventestatus_per_dag as (
+    select
+        ref_int_statuskoder_manuell_handtering.ventestatus_beskrivelse,
+        distinkte_beregninger_per_ventestatus.ventestatus_kode,
+        ref_stg_db2os__beregninger.beregnet_dato,
+        count(distinkte_beregninger_per_ventestatus.beregning_id)
+            as antall_beregninger,
+        case
+            when
+                ref_int_statuskoder_manuell_handtering.handteres_manuelt = 1
+                then 'Håndteres manuelt'
+            else 'Ingen manuell håndtering'
+        end as handteres_manuelt
+    from distinkte_beregninger_per_ventestatus
+    left join
+        ref_int_statuskoder_manuell_handtering
+        on
+            distinkte_beregninger_per_ventestatus.ventestatus_kode
+            = ref_int_statuskoder_manuell_handtering.ventestatus_kode
+    left join
+        ref_stg_db2os__beregninger
+        on
+            distinkte_beregninger_per_ventestatus.beregning_id
+            = ref_stg_db2os__beregninger.beregning_id
+    group by
+        ref_int_statuskoder_manuell_handtering.ventestatus_beskrivelse,
+        distinkte_beregninger_per_ventestatus.ventestatus_kode,
+        ref_int_statuskoder_manuell_handtering.handteres_manuelt,
+        ref_stg_db2os__beregninger.beregnet_dato
+),
+
+final as (
+    select
+        ventestatus_kode,
+        ventestatus_beskrivelse,
+        antall_beregninger,
+        handteres_manuelt
+    from antall_beregninger_per_ventestatus_per_dag
 )
 
-select
-    ventestatus_beskrivelse,
-    sts.ventestatus_kode,
-    beregnet_dato,
-    count(sts.beregning_id) as antall_beregninger,
-    case
-        when handteres_manuelt = 1 then 'Håndteres manuelt'
-        else 'Ingen manuell håndtering'
-    end as handteres_manuelt
-from distinkte_beregninger_per_ventestatus as sts
-left join
-    {{ ref('int_statuskoder_manuell_handtering') }} as stk
-    on sts.ventestatus_kode = stk.ventestatus_kode
-left join
-    {{ ref('stg_db2os__beregninger') }} as ber
-    on sts.beregning_id = ber.beregning_id
-group by
-    ventestatus_beskrivelse,
-    sts.ventestatus_kode,
-    handteres_manuelt,
-    beregnet_dato
+select * from final
