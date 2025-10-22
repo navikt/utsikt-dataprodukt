62ff524a8b620d3c91e76d780d73bd1e31e3f248
caroliwi <caroline.winther@nav.no>
2025-10-22 14:00:37 +0200

    legger til en tabell med stoppstatus og lengde basert på snapshot

---
+++ dbt_utsikt/models/marts/fak/fak_stoppstatus.sql
@@ -0,0 +1,52 @@
+--fak_stoppstatus
+with
+ref_stoppstatus_snapshot as (
+    select
+        beregning_id,
+        stoppniva_id,
+        ventestatus_kode,
+        registrert_tidspunkt,
+        dbt_valid_from as gyldig_fra_tid,
+        dbt_valid_to as gyldig_til_tid
+    from {{ ref('stoppstatus_snapshot') }}
+),
+
+ref_int_statuskoder_manuell_handtering as (
+    select
+        ventestatus_kode,
+        ventestatus_beskrivelse,
+        handteres_manuelt
+    from {{ ref('int_statuskoder_manuell_handtering') }}
+),
+
+beregne_lengde_stoppstatus as (
+    select
+        ref_stoppstatus_snapshot.beregning_id,
+        ref_stoppstatus_snapshot.stoppniva_id,
+        ref_stoppstatus_snapshot.ventestatus_kode,
+        ref_int_statuskoder_manuell_handtering.ventestatus_beskrivelse,
+        ref_int_statuskoder_manuell_handtering.handteres_manuelt as handteres_manuelt_flagg,
+        ref_stoppstatus_snapshot.registrert_tidspunkt,
+        ref_stoppstatus_snapshot.gyldig_fra_tid,
+        ref_stoppstatus_snapshot.gyldig_til_tid,
+        coalesce(ref_stoppstatus_snapshot.gyldig_til_tid, current_timestamp()) - ref_stoppstatus_snapshot.gyldig_fra_tid as lengde_stoppstatus
+    from ref_stoppstatus_snapshot
+    left join ref_int_statuskoder_manuell_handtering
+        on ref_stoppstatus_snapshot.ventestatus_kode = ref_int_statuskoder_manuell_handtering.ventestatus_kode
+),
+
+final as (
+    select
+        beregning_id,
+        stoppniva_id,
+        ventestatus_kode,
+        ventestatus_beskrivelse,
+        registrert_tidspunkt,
+        gyldig_fra_tid,
+        gyldig_til_tid,
+        handteres_manuelt_flagg,
+        lengde_stoppstatus
+    from beregne_lengde_stoppstatus
+)
+
+select * from final
