ce26853f18c78e735b871d58e0c94cd2bdea1bf6
caroliwi <caroline.winther@nav.no>
2025-12-02 10:33:00 +0100

    :memo: lagt til kolonne og tabellbeskrivelser fra kildetabeller

--- dbt_utsikt/models/intermediate/int_stoppstatuskoder_manuell_handtering.sql
+++ dbt_utsikt/models/intermediate/int_stoppstatuskoder_manuell_handtering.sql
@@ -1,24 +1,45 @@
-select
-    ventestatus_kode,
-    ventestatus_beskrivelse,
-    case
-        when
-            ventestatus_kode in (
-                'ADDR',
-                'ANRE',
-                'AVAG',
-                'AVAV',
-                'AVRK',
-                'AVVE',
-                'AVVM',
-                'EONK',
-                'EOPK',
-                'KRAV',
-                'OVUR',
-                'RETN',
-                'RETU'
-            )
-            then 1
-        else 0
-    end as handteres_manuelt
-from {{ ref('stg_db2os__stoppstatuskoder') }}
+with
+
+ref_stg_db2os__stoppstatuskoder as (
+    select
+        ventestatus_kode,
+        ventestatus_beskrivelse
+    from {{ ref('stg_db2os__stoppstatuskoder') }}
+),
+
+sette_manuelle_koder as (
+    select
+        ventestatus_kode,
+        ventestatus_beskrivelse,
+        case
+            when
+                ventestatus_kode in (
+                    'ADDR',
+                    'ANRE',
+                    'AVAG',
+                    'AVAV',
+                    'AVRK',
+                    'AVVE',
+                    'AVVM',
+                    'EONK',
+                    'EOPK',
+                    'KRAV',
+                    'OVUR',
+                    'RETN',
+                    'RETU'
+                )
+                then 1
+            else 0
+        end as handteres_manuelt
+    from ref_stg_db2os__stoppstatuskoder
+),
+
+final as (
+    select
+        ventestatus_kode,
+        ventestatus_beskrivelse,
+        handteres_manuelt
+    from sette_manuelle_koder
+)
+
+select * from final
