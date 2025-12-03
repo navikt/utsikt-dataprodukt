76c829a68e2fbdd1d1f5f0b007593e15c4f4a7ff
Linda J. Claesson <linda.josephine.claesson@nav.no>
2025-12-03 15:52:04 +0100

    :white_check_mark: generic test for eldre enn to år gammelt, lagt til for fak tabeller

--- dbt_utsikt/tests/eldre_enn_to_ar_gammelt.sql
+++
@@ -1,2 +0,0 @@
-select * from venteregister.antall_beregninger_per_fagomrade_per_dag --noqa: AM04
-where beregnet_dato < date_add(current_date(), interval -2 year)
---
+++ dbt_utsikt/tests/generic/eldre_enn_to_ar_gammelt.sql
@@ -0,0 +1,4 @@
+{% test eldre_enn_to_ar_gammelt(model, column_name) %}
+    select * from {{ model }} 
+    where cast({{ column_name }} AS DATE) < date_add(current_date(), interval -2 year)
+{% endtest %}
