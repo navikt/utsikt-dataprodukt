4b16df7e5f4bfa5e5b85938f3bc3faac74fdd3e6
caroliwi <caroline.winther@nav.no>
2025-12-04 12:23:07 +0100

    :recycle: gjort om view ventestatus_lengde_per_stoppniva til å basere på fak_stoppniva. samt fjernet lengde_stoppstatus fra fak_stoppstatus

--- dbt_utsikt/models/marts/agg/ventestatus_lengde_per_stoppniva.sql
+++ dbt_utsikt/models/marts/agg/ventestatus_lengde_per_stoppniva.sql
@@ -1,72 +1,37 @@
-with registrerte_tidsstempler as (
-    select
-        t1.beregning_id,
-        t1.stoppniva_id,
-        t1.lopenummer,
-        t1.ventestatus_kode,
-        t1.lastet_tid_kilde,
-        t2.lastet_tid_kilde as lastet_tid_kilde_neste_ventestatus,
-        t3.lastet_tid_kilde as lastet_tid_kilde_gjeldende_ventestatus
-    from {{ ref('stg_db2os__stoppstatuser') }} as t1
-    left join {{ ref('stg_db2os__stoppstatuser') }} as t2
-        on
-            t1.beregning_id = t2.beregning_id
-            and t1.stoppniva_id = t2.stoppniva_id
-            and t2.lopenummer = t1.lopenummer + 1
-    left join {{ ref('stg_db2os__stoppstatuser') }} as t3
-        on
-            t1.beregning_id = t3.beregning_id
-            and t1.stoppniva_id = t3.stoppniva_id
-            and t3.lopenummer = 9999
-),
+with
 
-lengder_stoppstatuser as (
+ref_fak_stoppstatus as (
     select
         beregning_id,
         stoppniva_id,
-        lopenummer,
         ventestatus_kode,
+        ventestatus_beskrivelse,
         lastet_tid_kilde,
-        case
-            when
-                lastet_tid_kilde_neste_ventestatus is not null
-                then
-                    lastet_tid_kilde_neste_ventestatus
-                    - lastet_tid_kilde
-            when
-                lopenummer = 9999
-                then
-                    cast(current_datetime('Europe/Oslo') as timestamp)
-                    - lastet_tid_kilde
-            else
-                lastet_tid_kilde_gjeldende_ventestatus
-                - lastet_tid_kilde
-        end as lengde_lopenummer
-    from registrerte_tidsstempler
+        gyldig_fra_tid,
+        gyldig_til_tid,
+        handteres_manuelt_flagg
+    from {{ ref("fak_stoppstatus") }}
+),
+
+beregne_lengde_stoppstatus as (
+    select
+        *,
+        coalesce(ref_fak_stoppstatus.gyldig_til_tid, current_timestamp()) - ref_fak_stoppstatus.gyldig_fra_tid as lengde_stoppstatus
+    from ref_fak_stoppstatus
+),
+
+final as (
+    select
+        beregning_id,
+        stoppniva_id,
+        ventestatus_kode,
+        ventestatus_beskrivelse,
+        lastet_tid_kilde,
+        gyldig_fra_tid,
+        gyldig_til_tid,
+        handteres_manuelt_flagg,
+        lengde_stoppstatus
+    from beregne_lengde_stoppstatus
 )
 
-select
-    beregning_id,
-    stoppniva_id,
-    lopenummer,
-    sts.ventestatus_kode,
-    ventestatus_beskrivelse,
-    lastet_tid_kilde,
-    lengde_lopenummer,
-    case
-        when handteres_manuelt = 1 then 'Håndteres manuelt'
-        else 'Ingen manuell håndtering'
-    end as handteres_manuelt,
-    --antar at ingen intervals har registrert noe større enn timer
-    round(
-        (
-            extract(hour from lengde_lopenummer)
-            + (extract(minute from lengde_lopenummer) / 60)
-            + (extract(second from lengde_lopenummer) / 3600)
-        ),
-        3
-    ) as lengde_antall_timer
-from
-    lengder_stoppstatuser as sts
-left join {{ ref('int_stoppstatuskoder_manuell_handtering') }} as stn
-    on sts.ventestatus_kode = stn.ventestatus_kode
+select * from final
--- dbt_utsikt/models/marts/fak/fak_stoppstatus.sql
+++ dbt_utsikt/models/marts/fak/fak_stoppstatus.sql
@@ -40,7 +40,7 @@ ref_int_stoppstatuskoder_manuell_handtering as (
     from {{ ref('int_stoppstatuskoder_manuell_handtering') }}
 ),
 
-beregne_lengde_stoppstatus as (
+join_manuell_handtering as (
     select
         ref_stoppstatus_snapshot.beregning_id,
         ref_stoppstatus_snapshot.stoppniva_id,
@@ -49,8 +49,7 @@ beregne_lengde_stoppstatus as (
         ref_int_stoppstatuskoder_manuell_handtering.handteres_manuelt as handteres_manuelt_flagg,
         ref_stoppstatus_snapshot.lastet_tid_kilde,
         ref_stoppstatus_snapshot.gyldig_fra_tid,
-        ref_stoppstatus_snapshot.gyldig_til_tid,
-        coalesce(ref_stoppstatus_snapshot.gyldig_til_tid, current_timestamp()) - ref_stoppstatus_snapshot.gyldig_fra_tid as lengde_stoppstatus
+        ref_stoppstatus_snapshot.gyldig_til_tid
     from ref_stoppstatus_snapshot
     left join ref_int_stoppstatuskoder_manuell_handtering
         on ref_stoppstatus_snapshot.ventestatus_kode = ref_int_stoppstatuskoder_manuell_handtering.ventestatus_kode
@@ -65,9 +64,8 @@ final as (
         lastet_tid_kilde,
         gyldig_fra_tid,
         gyldig_til_tid,
-        handteres_manuelt_flagg,
-        lengde_stoppstatus
-    from beregne_lengde_stoppstatus
+        handteres_manuelt_flagg
+    from join_manuell_handtering
 )
 
 select * from final
