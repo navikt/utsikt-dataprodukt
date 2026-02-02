1a9139b158408024b6f0caded7cee843366760a7
Linda J. Claesson <linda.josephine.claesson@nav.no>
2026-02-02 10:27:45 +0100

    :truck: kolonnenavn mnd til maned

--- dbt_utsikt/models/marts/agg/belop_per_fagomrade_per_maned.sql
+++ dbt_utsikt/models/marts/agg/belop_per_fagomrade_per_maned.sql
@@ -13,7 +13,7 @@ final as (
     select
         fagomrade_navn,
         faggruppe_navn,
-        extract(month from lastet_tid_kilde) as mnd,
+        extract(month from lastet_tid_kilde) as maned,
         extract(year from lastet_tid_kilde) as ar,
         sum(belop_brutto) as total_belop_brutto
     from ref_fak_stoppnivaer
