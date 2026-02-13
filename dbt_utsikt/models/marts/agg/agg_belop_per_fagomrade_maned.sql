7ea0f3704374fc42c03807d1a47052d3b7bfebaa
Linda J. Claesson <linda.josephine.claesson@nav.no>
2026-02-13 15:35:35 +0100

    :art: forste_i_maned som date istedenfor timestamp i agg_belop

--- dbt_utsikt/models/marts/agg/agg_belop_per_fagomrade_maned.sql
+++ dbt_utsikt/models/marts/agg/agg_belop_per_fagomrade_maned.sql
@@ -14,7 +14,7 @@ beregne_belop as (
     select
         fagomrade_navn,
         faggruppe_navn,
-        date_trunc(lastet_tid_kilde, month) as forste_i_maned,
+        cast(date_trunc(lastet_tid_kilde, month) as date) as forste_i_maned,
         sum(belop_brutto) as belop_brutto
     from ref_fak_stoppnivaer
     group by fagomrade_navn, faggruppe_navn, forste_i_maned
