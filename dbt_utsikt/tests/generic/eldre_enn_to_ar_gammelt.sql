26d52e07780dcbf47886a47774c5e884ce47f6da
caroliwi <caroline.winther@nav.no>
2026-03-09 11:25:21 +0100

    :wrench: endret test til å sjekke om data er eldre enn 731 dager istedet for 2 år

--- dbt_utsikt/tests/generic/eldre_enn_to_ar_gammelt.sql
+++ dbt_utsikt/tests/generic/eldre_enn_to_ar_gammelt.sql
@@ -1,4 +1,4 @@
 {% test eldre_enn_to_ar_gammelt(model, column_name) %}
     select * from {{ model }} 
-    where cast({{ column_name }} AS DATE) < date_add(current_date(), interval -2 year)
+    where cast({{ column_name }} AS DATE) < date_add(current_date(), interval -731 day)
 {% endtest %}
