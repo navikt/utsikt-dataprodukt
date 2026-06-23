e9a664f5e8889ddb10ac8aecc92698709e0e38c9
caroliwi <caroline.winther@nav.no>
2026-06-23 11:03:32 +0200

    lagt til flere kolonner i agg_oppdrag_xx

--- dbt_utsikt/models/marts/agg/agg_oppdrag_per_ytelse_kilde_dag.sql
+++ dbt_utsikt/models/marts/agg/agg_oppdrag_per_ytelse_kilde_dag.sql
@@ -1,70 +1,70 @@
 --agg_oppdrag_per_ytelse_kilde_dag
 
-with ref_agg_oppdrag_per_kilde_fagomrade_dag as (
+with ref_fak_oppdrag as (
     select
+        oppdrag_id,
+        fagomrade_kode,
         fagomrade_navn,
+        faggruppe_kode,
+        faggruppe_navn,
         kildesystem,
-        dato_oppdrag_lastet,
-        antall_oppdrag
-    from {{ ref('agg_oppdrag_per_kilde_fagomrade_dag') }}
+        lastet_tid_kilde,
+        lastet_tid
+    from {{ ref('fak_oppdrag') }}
 ),
 
-derive_ytelse as (
+agg_oppdrag_per_kilde_fagomrade_dag as (
     select
-        dato_oppdrag_lastet,
-        antall_oppdrag,
-        coalesce(kildesystem, 'Oppdragssystemet') as kildesystem,
-        case
-            when fagomrade_navn like '%Arbeidsavklaringspenger%'
-                then 'Arbeidsavklaringspenger'
-            when fagomrade_navn like '%Dagpenger%'
-                then 'Dagpenger'
-            when fagomrade_navn like '%Tiltakspenger%'
-                then 'Tiltakspenger'
-            when fagomrade_navn like '%Tilleggsstønad%'
-                then 'Tilleggsstønad'
-            when fagomrade_navn like '%Barnetrygd%'
-                then 'Barnetrygd'
-            when fagomrade_navn like '%Enslig forsørger%'
-                then 'Enslig forsørger'
-            when fagomrade_navn like '%Gravferdstønad%' or fagomrade_navn like '%Gravferdsstønad%'
-                then 'Gravferdsstønad'
-            when fagomrade_navn like '%Grunnstønad%' or fagomrade_navn like '%Hjelpestønad%'
-                then 'Grunn og hjelpestønad'
-            when fagomrade_navn like '%Uføretrygd%'
-                then 'Uføretrygd'
-            when fagomrade_navn like '%Alderspensjon%'
-                then 'Alderspensjon'
-            when fagomrade_navn like '%Uførepensjon fra SPK%'
-                then 'Uførepensjon fra SPK'
-            when fagomrade_navn like '%Kontantstøtte%'
-                then 'Kontantstøtte'
-            when fagomrade_navn like '%Sykepenger%'
-                then 'Sykepenger'
-            when fagomrade_navn like '%Foreldrepenger%'
-                then 'Foreldrepenger'
-            else 'Annet'
-        end as ytelse
-    from ref_agg_oppdrag_per_kilde_fagomrade_dag
+        fagomrade_kode,
+        fagomrade_navn,
+        faggruppe_kode,
+        faggruppe_navn,
+        kildesystem,
+        extract(date from lastet_tid_kilde) as dato_oppdrag_lastet,
+        count(distinct oppdrag_id) as antall_oppdrag
+    from ref_fak_oppdrag
+    group by
+        fagomrade_kode,
+        fagomrade_navn,
+        faggruppe_kode,
+        faggruppe_navn,
+        kildesystem,
+        extract(date from lastet_tid_kilde)
 ),
 
-agg_oppdrag_per_ytelse_kilde_dag as (
+ref_int_fagomrader_med_tilhorende_faggrupper as (
     select
-        ytelse,
-        kildesystem,
-        dato_oppdrag_lastet,
-        sum(antall_oppdrag) as antall_oppdrag
-    from derive_ytelse
-    group by ytelse, kildesystem, dato_oppdrag_lastet
+        fagomrade_kode,
+        ytelse
+    from {{ ref('int_fagomrader_med_tilhorende_faggrupper') }}
+),
+
+join_ytelse as (
+    select
+        agg_oppdrag_per_kilde_fagomrade_dag.fagomrade_kode,
+        agg_oppdrag_per_kilde_fagomrade_dag.fagomrade_navn,
+        agg_oppdrag_per_kilde_fagomrade_dag.faggruppe_kode,
+        agg_oppdrag_per_kilde_fagomrade_dag.faggruppe_navn,
+        agg_oppdrag_per_kilde_fagomrade_dag.dato_oppdrag_lastet,
+        agg_oppdrag_per_kilde_fagomrade_dag.antall_oppdrag,
+        ref_int_fagomrader_med_tilhorende_faggrupper.ytelse,
+        coalesce(agg_oppdrag_per_kilde_fagomrade_dag.kildesystem, 'Oppdragssystemet') as kildesystem
+    from agg_oppdrag_per_kilde_fagomrade_dag
+    left join ref_int_fagomrader_med_tilhorende_faggrupper
+        on agg_oppdrag_per_kilde_fagomrade_dag.fagomrade_kode = ref_int_fagomrader_med_tilhorende_faggrupper.fagomrade_kode
 ),
 
 final as (
     select
+        fagomrade_kode,
+        fagomrade_navn,
+        faggruppe_kode,
+        faggruppe_navn,
         ytelse,
         kildesystem,
         dato_oppdrag_lastet,
         antall_oppdrag
-    from agg_oppdrag_per_ytelse_kilde_dag
+    from join_ytelse
 )
 
 select * from final
